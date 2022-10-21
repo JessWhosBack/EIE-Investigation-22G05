d2796b0b92a386f516d5f4bd781aeba804e4b9c3
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2022-10-21 11:39:10 +0200

    latest fft code - now all graphs run (was a array size error)

--- Area/area.py
+++ Area/area.py
@@ -169,162 +169,173 @@ for image_counter, image in enumerate(image_array):
     # IMAGE PROCESSING TO GENERATE GRAPH OF HAND DRAWN LINE THAT HAS BEEN SHIFTED DOWN TO THE X-AXIS- - - - - - - - - - - #
     # NOTE: The horizontal axis is denoted by y, and the vertical axis is denoted by x (no, this was not on purpose)
     coordinates = np.argwhere(image < 0.9)
-    try:
-        x_tuple, y_tuple, z_tuple = zip(*coordinates)
+    # try:
+    x_tuple, y_tuple, z_tuple = zip(*coordinates)
 
-        x = np.array(x_tuple).tolist()
-        y = np.array(y_tuple).tolist()
-        z = np.array(z_tuple).tolist()
+    x = np.array(x_tuple).tolist()
+    y = np.array(y_tuple).tolist()
+    z = np.array(z_tuple).tolist()
 
-        average_x = np.mean(x)  
-        new_array_x = []
-        new_array_y = []
-        average_array_x = []
-        
-        # Sort the array of pixels into an ordered array according to the horizontal x-axis
-        for i in range(0, len(y)-1):
-            for j in range(0, len(y)-i-1):
-                if y[j] > y[j+1]:
-                    temp_x = x[j]
-                    x[j] = x[j+1]
-                    x[j+1] = temp_x
+    average_x = np.mean(x)  
+    new_array_x = []
+    new_array_y = []
+    average_array_x = []
+    
+    # Sort the array of pixels into an ordered array according to the horizontal x-axis
+    for i in range(0, len(y)-1):
+        for j in range(0, len(y)-i-1):
+            if y[j] > y[j+1]:
+                temp_x = x[j]
+                x[j] = x[j+1]
+                x[j+1] = temp_x
 
-                    temp_y = y[j]
-                    y[j] = y[j+1]
-                    y[j+1] = temp_y
+                temp_y = y[j]
+                y[j] = y[j+1]
+                y[j+1] = temp_y
 
-        # Take the absolute value and shift the points down to be centered around the vertical axis
-        for i in range(0, len(x)):
-            new_array_y.append(y[i])
-            average_array_x.append(0)
-            if x[i] < average_x: 
-                c = 3
-                new_array_x.append(average_x - x[i])
-            else:
-                new_array_x.append(x[i] - average_x)
+    # Take the absolute value and shift the points down to be centered around the vertical axis
+    for i in range(0, len(x)):
+        new_array_y.append(y[i])
+        average_array_x.append(0)
+        if x[i] < average_x: 
+            c = 3
+            new_array_x.append(average_x - x[i])
+        else:
+            new_array_x.append(x[i] - average_x)
 
-        # NOTE: The below code does NOT work as intended - it removes the top X% of all graphs, even those without erroneous border lines
-        # Need to try think of better solution! 
-        #
-        # # Remove erroneous X% of top pixels of image to get rid of any remaining border
-        # max_new_x = np.max(new_array_x)
-        # x_adjustment = 1/100*max_new_x
-        # tracker = 0
-        # for i in range(0, len(new_array_x)): 
-        #     if new_array_x[tracker] > max_new_x - x_adjustment:
-        #         new_array_y.pop(tracker)
-        #         new_array_x.pop(tracker)
-        #         tracker = tracker - 1
-        #     tracker = tracker + 1
+    # NOTE: The below code does NOT work as intended - it removes the top X% of all graphs, even those without erroneous border lines
+    # Need to try think of better solution! 
+    #
+    # # Remove erroneous X% of top pixels of image to get rid of any remaining border
+    # max_new_x = np.max(new_array_x)
+    # x_adjustment = 1/100*max_new_x
+    # tracker = 0
+    # for i in range(0, len(new_array_x)): 
+    #     if new_array_x[tracker] > max_new_x - x_adjustment:
+    #         new_array_y.pop(tracker)
+    #         new_array_x.pop(tracker)
+    #         tracker = tracker - 1
+    #     tracker = tracker + 1
 
-        # Plot the shifted points as a line graph 
-        # figure_area = plt.subplot(math.ceil(file_count/2), 2, image_counter+1)
-        # figure_area = plt.title(str(image_names[image_counter]))  
-        # figure_area = plt.plot(new_array_y, new_array_x)
-        # figure_area = plt.fill_between(new_array_y, new_array_x, color="grey")
-        # figure_area = plt.xlim(0, 600)
-        # figure_area = plt.ylim(0, 20)
+    figure_fft = plt.subplot(math.ceil(file_count/2), 2, image_counter+1)
+
+    # Plot the shifted points as a line graph 
+    # figure_area = plt.subplot(math.ceil(file_count/2), 2, image_counter+1)
+    # figure_area = plt.title(str(image_names[image_counter]))  
+    figure_fft = plt.plot(new_array_y, new_array_x)
+    # figure_area = plt.fill_between(new_array_y, new_array_x, color="grey")
+    # figure_area = plt.xlim(0, 600)
+    # figure_area = plt.ylim(0, 20)
 
 
-        sos = signal.iirfilter(4, Wn=[0.1, 2.5], fs=30, btype="bandpass", ftype="butter", output="sos")
-        yfilt = signal.sosfilt(sos, new_array_x)
+    sos = signal.iirfilter(4, Wn=[0.1, 2.5], fs=30, btype="bandpass", ftype="butter", output="sos")
+    yfilt = signal.sosfilt(sos, new_array_x)
 
-        # figure_area = plt.plot(yfilt)
+    # figure_area = plt.plot(yfilt)
 
-        figure_fft = plt.subplot(math.ceil(file_count/2), 2, image_counter+1)
-        # figure_fft = plt.xlim(0, 0.2)
-        data_step = 0.1
-        n = len(new_array_y)
-        yf = rfft(new_array_x)
-        xf = rfftfreq(n, data_step)
-        # figure_fft = plt.plot(xf, np.abs(yf))
+    # figure_fft = plt.xlim(0, 0.2)
+    data_step = 0.1
+    n = len(new_array_y)
+    yf = rfft(new_array_x)
+    xf = rfftfreq(n, data_step)
+    # figure_fft = plt.plot(xf, np.abs(yf))
 
-        yf_abs = np.abs(yf)
-        indices = yf_abs>500
-        yf_clean = indices*yf
-        # figure_fft = plt.plot(xf, np.abs(yf_clean))
+    yf_abs = np.abs(yf)
+    yf_max = np.max(yf_abs)
+    print(yf_max)
 
-        new_f_clean = irfft(yf_clean)
+    
+
+    indices = yf_abs > (5.0/100*yf_max)
+    yf_clean = indices*yf
+    print(yf_clean)
+    # figure_fft = plt.plot(xf, np.abs(yf_clean))
+
+    new_f_clean = irfft(yf_clean)
+    if len(new_array_y) > len(new_f_clean):
+        new_new_array_y = new_array_y
+        new_new_array_y.pop(0)
+        figure_fft = plt.plot(new_new_array_y, new_f_clean)
+    else:
         figure_fft = plt.plot(new_array_y, new_f_clean)
+    # EXTRACTING OF NUMERICAL DATA FROM THE ABOVE GRAPH - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Area under the curve, using numpy's trapz formula
+    total_area_trapz_x = round(trapz(new_array_x), 2)
+    results_array[image_counter][Results.AREA_TRAPZ.value] = round(total_area_trapz_x, 2)
+    figure_area = plt.text(400, 15, "Area: " + str(total_area_trapz_x), bbox=dict(facecolor='red', alpha=0.5))
 
-        # EXTRACTING OF NUMERICAL DATA FROM THE ABOVE GRAPH - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-        # Area under the curve, using numpy's trapz formula
-        total_area_trapz_x = round(trapz(new_array_x), 2)
-        results_array[image_counter][Results.AREA_TRAPZ.value] = round(total_area_trapz_x, 2)
-        figure_area = plt.text(400, 15, "Area: " + str(total_area_trapz_x), bbox=dict(facecolor='red', alpha=0.5))
+    # Quantile values of the data
+    min, q1, q2, q3, q90, max = np.quantile(new_array_x, [0, 0.25, 0.5, 0.75, 0.9, 1])
+    iqr = q3-q1
+    std = np.std(new_array_x)
+    # figure_area = plt.text(400, 10, "Q75: " + str(round(q3, 2)), bbox=dict(facecolor='red', alpha=0.5))
+    # figure_area = plt.text(400, 5, "IQR: " + str(round(iqr, 2)), bbox=dict(facecolor='red', alpha=0.5))
 
-        # Quantile values of the data
-        min, q1, q2, q3, q90, max = np.quantile(new_array_x, [0, 0.25, 0.5, 0.75, 0.9, 1])
-        iqr = q3-q1
-        std = np.std(new_array_x)
-        # figure_area = plt.text(400, 10, "Q75: " + str(round(q3, 2)), bbox=dict(facecolor='red', alpha=0.5))
-        # figure_area = plt.text(400, 5, "IQR: " + str(round(iqr, 2)), bbox=dict(facecolor='red', alpha=0.5))
+    ## NEW CODE HERE: 
+    num_peaks = signal.find_peaks(np.array(new_array_x), threshold=q3)
+    print(len(num_peaks[0]))
+    print(num_peaks)
 
-        ## NEW CODE HERE: 
-        num_peaks = signal.find_peaks(np.array(new_array_x), threshold=q3)
-        print(len(num_peaks[0]))
-        print(num_peaks)
-
-        # figure_area = plt.text(400, 20, "Num Peaks: " + str(len(num_peaks[0])), bbox=dict(facecolor='blue', alpha=0.5))
+    # figure_area = plt.text(400, 20, "Num Peaks: " + str(len(num_peaks[0])), bbox=dict(facecolor='blue', alpha=0.5))
 
 
-        ## END OF NEW CODE 
-
+    ## END OF NEW CODE 
 
 
 
 
 
-        # results_array[image_counter][Results.MIN.value] = round(min, 2)
-        # results_array[image_counter][Results.Q1.value] = round(q1, 2)
-        results_array[image_counter][Results.Q2.value] = round(q2, 2)
-        results_array[image_counter][Results.Q3.value] = round(q3, 2)
-        results_array[image_counter][Results.MAX.value] = round(max, 2)
-        # results_array[image_counter][Results.IQR.value] = round(iqr, 2)
-        results_array[image_counter][Results.STDDEV.value] = round(std, 2)
 
-        average_areas = []
-        temp_counter = 1
-        max_new_array_y = np.max(new_array_y)
-        percent_of_xaxis = int(5/100*max_new_array_y)
-        array_iterator = 0
-        
-        for i in range(percent_of_xaxis, max_new_array_y, percent_of_xaxis):
-            temp_array_x = []
-            temp_array_y = []
-            before_array_iterator = array_iterator
+    # results_array[image_counter][Results.MIN.value] = round(min, 2)
+    # results_array[image_counter][Results.Q1.value] = round(q1, 2)
+    results_array[image_counter][Results.Q2.value] = round(q2, 2)
+    results_array[image_counter][Results.Q3.value] = round(q3, 2)
+    results_array[image_counter][Results.MAX.value] = round(max, 2)
+    # results_array[image_counter][Results.IQR.value] = round(iqr, 2)
+    results_array[image_counter][Results.STDDEV.value] = round(std, 2)
 
-            while new_array_y[array_iterator] < i:
-                temp_array_x.append(new_array_x[array_iterator])
-                temp_array_y.append(new_array_y[array_iterator])
-                array_iterator = array_iterator + 1
+    average_areas = []
+    temp_counter = 1
+    max_new_array_y = np.max(new_array_y)
+    percent_of_xaxis = int(5/100*max_new_array_y)
+    array_iterator = 0
+    
+    for i in range(percent_of_xaxis, max_new_array_y, percent_of_xaxis):
+        temp_array_x = []
+        temp_array_y = []
+        before_array_iterator = array_iterator
 
-            if array_iterator == before_array_iterator:
-                temp_array_x.append(new_array_x[array_iterator-1])
-                temp_array_y.append(new_array_y[array_iterator-1])
-                temp_array_x.append(new_array_x[array_iterator])
-                temp_array_y.append(new_array_y[array_iterator])
-                        
-            temp_area_x = round(trapz(temp_array_x, temp_array_y), 2)
-            average_areas.append(temp_area_x)   
-            temp_counter = temp_counter + 1
+        while new_array_y[array_iterator] < i:
+            temp_array_x.append(new_array_x[array_iterator])
+            temp_array_y.append(new_array_y[array_iterator])
+            array_iterator = array_iterator + 1
 
-        # Quantile values of the data
-        avg_avg_area = np.mean(average_areas)
-        avg_min, avg_q1, avg_q2, avg_q3, avg_max = np.quantile(average_areas, [0, 0.25, 0.5, 0.75, 1])
-        avg_iqr = avg_q3-avg_q1
-        avg_std = np.std(average_areas)
+        if array_iterator == before_array_iterator:
+            temp_array_x.append(new_array_x[array_iterator-1])
+            temp_array_y.append(new_array_y[array_iterator-1])
+            temp_array_x.append(new_array_x[array_iterator])
+            temp_array_y.append(new_array_y[array_iterator])
+                    
+        temp_area_x = round(trapz(temp_array_x, temp_array_y), 2)
+        average_areas.append(temp_area_x)   
+        temp_counter = temp_counter + 1
 
-        results_array[image_counter][Results.AVG_AREA_TRAPZ.value] = round(avg_avg_area,2)
-        results_array[image_counter][Results.AVG_AREA_MIN.value] = round(avg_min,2)
-        # results_array[image_counter][Results.AVG_AREA_Q1.value] = round(avg_q1,2)
-        results_array[image_counter][Results.AVG_AREA_Q2.value] = round(avg_q2,2)
-        results_array[image_counter][Results.AVG_AREA_Q3.value] = round(avg_q3,2)
-        results_array[image_counter][Results.AVG_AREA_MAX.value] = round(avg_max,2)
-        # results_array[image_counter][Results.AVG_AREA_IQR.value] = round(avg_iqr,2)
-        results_array[image_counter][Results.AVG_AREA_STDDEV.value] = round(avg_std,2)
-    except:
-        print("ERROR")
+    # Quantile values of the data
+    avg_avg_area = np.mean(average_areas)
+    avg_min, avg_q1, avg_q2, avg_q3, avg_max = np.quantile(average_areas, [0, 0.25, 0.5, 0.75, 1])
+    avg_iqr = avg_q3-avg_q1
+    avg_std = np.std(average_areas)
+
+    results_array[image_counter][Results.AVG_AREA_TRAPZ.value] = round(avg_avg_area,2)
+    results_array[image_counter][Results.AVG_AREA_MIN.value] = round(avg_min,2)
+    # results_array[image_counter][Results.AVG_AREA_Q1.value] = round(avg_q1,2)
+    results_array[image_counter][Results.AVG_AREA_Q2.value] = round(avg_q2,2)
+    results_array[image_counter][Results.AVG_AREA_Q3.value] = round(avg_q3,2)
+    results_array[image_counter][Results.AVG_AREA_MAX.value] = round(avg_max,2)
+    # results_array[image_counter][Results.AVG_AREA_IQR.value] = round(avg_iqr,2)
+    results_array[image_counter][Results.AVG_AREA_STDDEV.value] = round(avg_std,2)
+    # except:
+    #     print("ERROR")
 
 figure_area = plt.tight_layout()
 figure_area = plt.show()
